d472341c897042c80f1f954b8528af9acd8ff744
Tomasz Podkowski <tomasz@podkow.ski>
2021-01-10 14:23:01 +0100

    Add auth and user modules.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   @Get()
   getHello(): string {
     return 'Welcome to Mealplanner API!';
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ProductsModule } from './products/products.module';
 import { RecipesModule } from './recipes/recipes.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProductsModule, RecipesModule],
+  imports: [ProductsModule, RecipesModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [],
 })
