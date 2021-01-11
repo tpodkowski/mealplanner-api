4d7976d417a9c4ca9361d769dbffea5021444f9d
Tomasz Podkowski <tomasz@podkow.ski>
2021-01-11 22:21:16 +0100

    Fix tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+
 import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { JwtStrategy } from './jwt.strategy';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [
+        ConfigModule.forRoot(),
+        UsersModule,
+        PassportModule,
+        JwtModule.register({
+          secret: 'secret',
+          signOptions: { expiresIn: '60d' },
+        }),
+      ],
+      providers: [AuthService, JwtStrategy],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
