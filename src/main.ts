eea532fcb7f2530161288b4f505131acc79cacf5
Tomasz Podkowski <tomasz@podkow.ski>
2021-01-14 11:53:51 +0100

    Add custom validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { CustomValidationPipe } from './common/pipes/ValidationPipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new CustomValidationPipe());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
